b79ec768a84298095994efe151497f3c416d3908
azanali506 <malikhuusain90@gmail.com>
2026-08-16 00:21:53 +0400

    Update BOT_TOKEN with actual BotFather token
    Removed placeholder text and added actual bot token.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 nest_asyncio.apply()
 
-# Yahan 'YOUR_BOT_TOKEN_HERE' ki jagah apna BotFather se mila hua Token likhein
-BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"8844936294:AAGXcIMgXGjBy_kz7XdydztQu4-E2wxLQhs
+# Yahan 'YOUR_BOT_TOKEN_HERE' ki jagah apna BotFather se mila hua TokenBOT_TOKEN = "8844936294:AAGXcIMgXGjBy_kz7XdydztQu4-E2wxLQhs"
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     welcome_text = (
